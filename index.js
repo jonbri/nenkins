367a8101d7822e928eac767ea732cc5f68dd5cfa
Jonathan Brink <jonathandavidbrink@gmail.com>
2016-09-29 13:18:01 -0400

    execute commands

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ var exec = require('child_process').exec;
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
-    exec("ls", function(error, stdout, stderr) {
-        res.end('stdout: ' + stdout);
+    var sResponse,
+        sFullCommand,
+        aParts,
+        sCommand,
+        aArgs = [];
+
+    aParts = req.url.split(/\//);
+    aParts.shift();
+
+    if (aParts[0] === '') {
+        res.end('No command to run');
+        return;
+    }
+
+    sCommand = aParts.shift();
+    aArgs = aParts.slice(0);
+
+    var sFullCommand = sCommand + ' ' + aArgs.toString().replace(',',' ');
+    exec(sFullCommand, function(error, stdout, stderr) {
+        sResponse = 'command: ' + sFullCommand;
+        sResponse += '\n\rstderr:\n\r' + stderr;
+        sResponse += '\n\rstdout:\n\r' + stdout;
+        res.end(sResponse);
     });
 }).listen(3123);
 console.log('Running on port 3123...');
